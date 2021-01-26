a82304fa8152dfe4c86fae542e5952deeb4ee352
Ryan Gross <ryan@pop-os.localdomain>
2021-01-25 23:00:25 -0500

    Fleshing out models a bit more

--- client/src/app/models/job.model.ts
+++ client/src/app/models/job.model.ts
@@ -1,3 +1,19 @@
+import { Equipment } from './equipment.model';
+import { User } from './user.model';
+
+export enum JobType {
+
+}
+
 export interface Job {
-    
+    id: string,
+    name: string,
+    description: string,
+    affectedEquipment: Equipment[],
+    reported: Date,
+    reportedBy: User,
+    start: Date,
+    end: Date,
+    operator: User,
+    comments: Comment[]
 }
--- client/src/app/models/notification.model.ts
+++ client/src/app/models/notification.model.ts
@@ -1,5 +1,12 @@
+export enum NotificationType {
+    Info = 1,
+    Warning = 2,
+    Error = 3
+}
+
+
 export interface Notification {
     id: string,
     type: string,
-    message: string
+    message: NotificationType
 }
