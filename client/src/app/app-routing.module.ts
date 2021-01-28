23ffb77c01360e17c7a147ecd00b13b511d68dac
Ryan Gross <ryan@pop-os.localdomain>
2021-01-28 17:19:14 -0500

    Adding routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OverviewComponent } from './components/overview/overview.component';
+import { UsersComponent } from './components/users/users.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'users', component: UsersComponent },
+  { path: '**', component: OverviewComponent },
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+ }
